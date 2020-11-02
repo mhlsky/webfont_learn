db21141806c191b978561868a9f9a662a9714604
admin <2608656305@qq.com>
2020-11-02 21:21:51 +0800

    webpack的基础配置

---
+++ webfront/二、前端模块化(webpack)/03-webpack的使用/02-webpack的配置/src/main.js
@@ -0,0 +1,9 @@
+const {add,mul} = require('./mathUtil')
+
+console.log(add(20,30));
+console.log(mul(20,30));
+import { name,age,height} from "./info"
+console.log(name);
+console.log(age);
+console.log(height);
+
