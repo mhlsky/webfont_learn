49d32e31b1d4b28a7462d15ce977ae5436262e5d
admin <2608656305@qq.com>
2020-11-13 19:32:15 +0800

    Vue in webpack ___update

---
+++ webfront/二、前端模块化(webpack)/04-webpack配置vue/src/main.js
@@ -0,0 +1,25 @@
+
+const {add,mul} = require('./js/mathUtil')
+
+console.log(add(20,30));
+console.log(mul(20,30));
+import { name,age,height} from "./js/info"
+console.log(name);
+console.log(age);
+console.log(height);
+
+//依赖css文件
+require('./css/normal.css') 
+
+//依赖less文件
+require('./css/speacial.less')
+document.writeln('<h2>你好，mhlsky!</h2>')
+//使用Vue开发
+import Vue from 'vue'
+
+new Vue({
+  el:'#app',
+  data:{
+    msg:'hello webpack'
+  }
+})
--- webfront/二、前端模块化(webpack)/04-webpack配置vue/webpack.config.js
+++ webfront/二、前端模块化(webpack)/04-webpack配置vue/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports={
   entry:'./src/main.js',
   output:{
@@ -5,4 +6,59 @@ module.exports={
     filename:'bundle.js',
     publicPath:'dist/'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        //css只负责家在不负责解析，也不负责生效
+        use: [ 'style-loader', 'css-loader', ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "less-loader", options: {
+                strictMath: true,
+                noIeCompat: true
+            }
+        }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              //当加载的图片小于limit时，加载为base64的字符串
+              limit: 15000,
+              name:'img/[name].[hash:8].[ext]'
+            },
+           
+
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015'],
+            
+          }
+        }
+      }
+     
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+    }
+  }
 }
